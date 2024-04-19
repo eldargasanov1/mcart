75028a51ec2de163ad41e641bdd3772053263fca
Eldar Həsənov <eldarforlife1@gmail.com>
2024-04-19 22:53:23 +0300

    task №162558

--- o-servise/kontakty/obratnaya-svyaz.php
+++ o-servise/kontakty/obratnaya-svyaz.php
@@ -1,15 +1,34 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Обратная связь");
-?><b>Обратная связь</b><br>
-<?$APPLICATION->IncludeComponent(
-	"bitrix:main.feedback",
-	"",
-	Array(
-		"EMAIL_TO" => "eldarforwork1@gmail.com",
-		"EVENT_MESSAGE_ID" => array(),
-		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
-		"REQUIRED_FIELDS" => array(),
-		"USE_CAPTCHA" => "Y"
-	)
-);?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>
+    <div class="site-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-8 mb-5">
+                <? $APPLICATION->IncludeComponent(
+                    "home:main.feedback",
+                    "",
+                    array(
+                        "EMAIL_TO" => "eldarforwork1@gmail.com",
+                        "EVENT_MESSAGE_ID" => array("7"),
+                        "OK_TEXT" => "Спасибо, ваше сообщение принято.",
+                        "REQUIRED_FIELDS" => array("NAME", "EMAIL", "SUBJECT", "MESSAGE"),
+                        "USE_CAPTCHA" => "Y"
+                    )
+                ); ?>
+            </div>
+            <? $APPLICATION->IncludeComponent(
+                "bitrix:main.include",
+                "",
+                array(
+                    "AREA_FILE_SHOW" => "file",
+                    "AREA_FILE_SUFFIX" => "inc",
+                    "EDIT_TEMPLATE" => "",
+                    "PATH" => "/include/contact_info.php"
+                )
+            ); ?>
+        </div>
+    </div>
+    </div>
+    <br><? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
