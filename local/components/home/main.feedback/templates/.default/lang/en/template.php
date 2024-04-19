f165b2933b9970641e02fcbef83d228f6dd9fb71
Eldar Həsənov <eldarforlife1@gmail.com>
2024-04-19 22:56:46 +0300

    task №162558 | fix

--- local/components/home/main.feedback/templates/.default/lang/en/template.php
+++ local/components/home/main.feedback/templates/.default/lang/en/template.php
@@ -1,6 +1,7 @@
 <?
 $MESS ['MFT_NAME'] = "Name";
 $MESS ['MFT_EMAIL'] = "Your E-mail";
+$MESS ['MFT_SUBJECT'] = "Your Subject";
 $MESS ['MFT_MESSAGE'] = "Message";
 $MESS ['MFT_CAPTCHA'] = "CAPTCHA";
 $MESS ['MFT_CAPTCHA_CODE'] = "Type the letters you see on the picture";
